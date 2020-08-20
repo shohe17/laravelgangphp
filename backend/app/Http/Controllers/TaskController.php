9ba8045e8cc3a76b0fe0429e5bd5aa94a17c377c
Shohei Horigo <shoheihorigo@ShoheinoMacBook-Air.local>
2020-08-20 11:23:02 +0900

    (WIP)タスクフォルダの画面表示

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+        //第一関数がテンプレートファイル名
+        //第二関数がテンプレートに渡すデータ  
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
