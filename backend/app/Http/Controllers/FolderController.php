6be990979a25211da0cb3f7ff2130f84123e9384
Shohei Horigo <shoheihorigo@ShoheinoMacBook-Air.local>
2020-08-24 15:14:00 +0900

    (wip)ファイル追加機能

---
+++ backend/app/Http/Controllers/FolderController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
+
+use function Symfony\Component\String\s;
+
+class FolderController extends Controller
+{
+  public function showCreateForm()
+  {
+    return view('folders/create');
+  }
+
+  //引数にインポートしたrequestクラスを入れる
+  public function create(CreateFolder $request)
+  {
+    // dd($request);
+    //フォルダモデルのインスタンすを作成する
+    $folder = new Folder();
+    //タイトルに入力値を代入する
+    $folder->title = $request->title;
+    //インスタンスの状態をdbに書き込む
+    $folder->save();
+
+    //リダイレクト？
+    return redirect()->route('tasks.index', [
+      'id' => $folder->id,
+      
+    ]);
+  }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,9 @@
 //ポイントは{}内のidで、表示したいタスクによってidが変わる仕組み
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
+//getで/folders/createにリクエストがきたら、FolderControllerでshowCreateFormメソッドを呼び出す
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+
+//postで/folders/createからデータを送信する。FolderControllerでcreateメソッドを呼び出す
+Route::post('/folders/create', 'FolderController@create');
+
